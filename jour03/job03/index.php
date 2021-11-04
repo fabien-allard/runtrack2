e5bc51cabe75e5e74026595f633008bb9c095057
fabien-allard <91756104+fabien-allard@users.noreply.github.com>
2021-11-04 19:45:09 +0100

    add index.php

--- jour03/job03/index.php
+++ jour03/job03/index.php
@@ -1,1 +1,16 @@
+<?php
 
+$str ="I'm sorry Dave I'm afraid I can't do that.";
+
+$Lettres= false;
+
+for ($i = 0; isset($str[$i]); $i++)
+    if ($str[$i] == "a" OR  $str[$i] == "e" OR $str[$i] == "i" OR $str[$i] == "o" OR $str[$i] == "u" OR $str[$i] == "y" OR
+        $str[$i] == "A" OR $str[$i] == "E" OR $str[$i] == "I" OR $str[$i] == "O" OR $str[$i] == "U" OR $str[$i] == "Y"){
+    echo $str[$i];
+}
+
+else {
+    echo $Lettres;
+}
+?>
