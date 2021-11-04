bb97dd733e20b1f27c57fd1590d0d9b9339bdba9
fabien-allard <91756104+fabien-allard@users.noreply.github.com>
2021-11-04 18:15:03 +0100

    add job05 index.php

--- jour02/job05/index.php
+++ jour02/job05/index.php
@@ -1,1 +1,20 @@
+<?php
+$Dernier_Nombres = 1000;
 
+    for ($j = 2;  $j <= $Dernier_Nombres; $j++){
+    $np = 1;
+}
+{
+
+    for ($i = 2; $i <= $i; $j++)
+    {
+        if (($j%$i) == 0)
+        {
+            $np = 0;
+        }
+    }
+
+            if ($np ==1)
+                echo $j. "";
+}
+?>
