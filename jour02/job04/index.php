352ba76d698eff977eaaf5acbbd061444c3dc356
fabien-allard <91756104+fabien-allard@users.noreply.github.com>
2021-11-04 18:13:42 +0100

    add job 04 index.php

--- jour02/job04/index.php
+++ jour02/job04/index.php
@@ -1,1 +1,23 @@
+<?php
 
+for ($n=1 ; $n <=100 ; $n++)
+    if ($n >= 1 && $n <=20){
+        echo "$n", "<br>";
+    }
+
+    elseif ($n %3 && $n%5){
+        echo "FizzBuzz", "<br>";
+    }
+
+        elseif ($n %3){
+            echo "Buzz", "<br>";
+        }
+
+            elseif ($n >= 40 && $n <=60){
+                echo "n", "<br>";
+            }
+                elseif ($n %5){
+                    echo "Fizz", "<br>";
+                }  
+
+?>
