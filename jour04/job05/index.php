60616f97175a2870bf79bb25197e2887b2d436e2
fabien-allard <91756104+fabien-allard@users.noreply.github.com>
2021-11-05 22:27:28 +0100

    add upload index.php

--- jour04/job05/index.php
+++ jour04/job05/index.php
@@ -1,1 +1,34 @@
+<html>
+    <body> 
+        
+        <?php
+                if($_POST['username']=="john" && $_POST['password']=="Rambo")
+                {
+                        echo  "<br/>C'est Pas ma guerre";
+                }
+                else{
+                    echo  "<br/>Votre pire Cauchemar";
+                }
+            
+                ?>
 
+        <div id="container">
+            <!-- zone de connexion -->
+            <center>
+            <form action="index.php" method="POST">
+                <h1>Connexion</h1>
+                
+                <label><b>Nom d'utilisateur</b></label>
+                <input type="text" name="username" placeholder="Entrer le nom d'utilisateur" request>
+                <br/><br/>
+                <label><b>Mot de passe</b></label>
+                <input type="password" name="password" placeholder="Entrer le mot de passe" request >
+                <br/><br/>
+                <input type="submit" name='envoie' value='Se Connecter' request >
+                <center>
+
+        
+            </form>
+        </div>
+    </body>
+</html>
