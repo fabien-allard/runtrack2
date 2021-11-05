d05418695879c9668759ff4797d143c22a0bc592
fabien-allard <91756104+fabien-allard@users.noreply.github.com>
2021-11-05 22:11:54 +0100

    add upload files

--- jour04/job03/index.php
+++ jour04/job03/index.php
@@ -1,1 +1,39 @@
+<?php
 
+    foreach($_POST as $value){
+    echo $value."<br/>";
+}
+?>
+
+
+
+
+<!-- Debut du formulaire -->
+<center>
+<form  action="./index.php" method="Post">
+        <legend>Formulaire :</legend>
+        <br /> <br />
+        Pseudo : <input type="text" name="Pseudo" value="">
+        <br /><br/>
+        Nom : <input type="text" name="Nom" value="">
+        <br /><br/>
+    Prénom : <input type="text" name="prenom" value="">
+    <br /><br/>
+
+    Sexe : 
+    <input type="radio" name="sexe" value="F">Femme
+    <input type="radio" name="sexe" value="H">Homme
+    <br /><br/>
+    Pays :
+    <select name="pays">
+        <option value="France">France</option>
+        <option value="Belgique">Belgique</option>
+        <option value="Suisse">Suisse</option>
+        <option value="Canada">Canada</option>
+</select>
+<br /><br/>
+
+<input type="submit" value="Envoyer">
+<br />
+</form>
+<center>
