d7c256b0f40fa0a9a745607862af0d21f5088b00
fabien-allard <91756104+fabien-allard@users.noreply.github.com>
2021-11-05 22:28:21 +0100

    add upload index.php job06

--- jour04/job06/index.php
+++ jour04/job06/index.php
@@ -1,1 +1,30 @@
+<html>
+    <body> 
+        
 
+        <div id="container">
+            <!-- zone de connexion -->
+            <center>
+            <form action="index.php" method="GET">
+                <h1>Nombres Pairs/impairs</h1>
+                
+                <label><b>Nombres :</b></label>
+                <input type="text" name="nombre">
+                <br/><br/>
+                <input type="submit" name='submit' value='Envoyer' request >
+                <center>
+
+<?php
+        if (isset($_GET['nombre'])){
+            if($_GET['nombre'] %2 ==0)
+            {
+                echo "<br/>Nombres pairs";
+        }
+        else{
+            echo "<br/>Nombres impairs";
+        }
+    }
+?>
+</div>
+    </body>
+</html>
