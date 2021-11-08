dd7ed520a631da7df959986141130f6a4ab20cb7
fabien-allard <91756104+fabien-allard@users.noreply.github.com>
2021-11-08 20:49:11 +0100

    upload index.php job04

--- jour05/job04/index.php
+++ jour05/job04/index.php
@@ -1,1 +1,32 @@
+<?php
 
+function calculate($a, $operation, $b)
+{
+    if($operation=="-")
+    return $a-$b;
+
+    elseif ($operation=="*"){
+        return $a*$b;
+    }
+
+    elseif ($operation=="+"){
+        return $a+$b;
+    }
+
+    elseif ($operation=="%"){
+        return $a%$b;
+    }
+
+    elseif ($operation=="/"){
+        return $a/$b;
+    }
+}
+
+
+echo calculate(8,"+",5);
+echo "<br/>" ,calculate(26,"-",15);
+echo "<br/>" ,calculate(26,"*",4);
+echo "<br/>" ,calculate(26,"%",6);
+echo "<br/>" ,calculate(26,"/",8);
+
+?>
