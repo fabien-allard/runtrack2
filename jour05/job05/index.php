a2a7dc96af81c6904713c81a18fdd1096d3c4fa1
fabien-allard <91756104+fabien-allard@users.noreply.github.com>
2021-11-08 20:50:02 +0100

    upload index.php in job05

--- jour05/job05/index.php
+++ jour05/job05/index.php
@@ -1,1 +1,21 @@
+<?php
 
+function occurrences( string $str, $char){
+
+    $j=0;
+
+        for($i =0; isset($str[$i]);
+        $i++){
+
+        if ($str [$i] ==$char){
+        $j++;
+    }
+}
+
+
+return $j;
+}
+echo occurrences ("Bienvenue sur Marseille","e");
+echo "<br>", occurrences("Travaux","r");
+
+?>
