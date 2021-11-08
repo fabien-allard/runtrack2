9a96701e2dd1acb3de439f730baa8a0b7f88cdc6
fabien-allard <91756104+fabien-allard@users.noreply.github.com>
2021-11-08 20:43:25 +0100

    upload index.php

--- jour05/job02/index.php
+++ jour05/job02/index.php
@@ -1,1 +1,15 @@
+<?php
+$jour=Null;
 
+function bonjour($jour){
+if ($jour==true){
+    echo "Bonjour<br>";
+}
+
+elseif($jour==false){
+    echo "Bonsoir<br>";
+    }
+}
+bonjour(true);
+bonjour(false);
+?>
