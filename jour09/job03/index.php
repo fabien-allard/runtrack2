f7d4928b63bc16ff14a652d205cb84d28a1df48f
fabien-allard <91756104+fabien-allard@users.noreply.github.com>
2021-11-22 23:48:30 +0100

    Add files via upload
    upload job03 index

--- jour09/job03/index.php
+++ jour09/job03/index.php
@@ -1,1 +1,42 @@
+<?php
+$bdd = mysqli_connect('localhost','root','', 'jour08');
+mysqli_set_charset($bdd, 'utf8');
+$requete = mysqli_query($bdd,"SELECT prenom, nom, naissance  FROM etudiants WHERE sexe = 'femme'");
+$etudiants = mysqli_fetch_all($requete, MYSQLI_ASSOC);
+?>
 
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Table&Requete</title>
+</head>
+<body>
+    <center>
+    <table cellspacing="5", border=3>
+        <thead>
+            
+            <th>Nom</th>
+            <th>Prenom</th>
+            <th>naissance</th>
+    
+    </thead>
+
+
+<tbody>
+<?php
+foreach($etudiants as $etudiant){
+echo '<tr>';
+    foreach($etudiant as $infos => $value){
+        echo '<td>'.$value.'</td>';
+    }
+echo '</tr>';
+}
+?>
+<center>
+</tbody>
+</table>
+</html>
