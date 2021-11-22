bbb8e9fb07d964d3d1a3edc1a6ea1ca53051145c
fabien-allard <91756104+fabien-allard@users.noreply.github.com>
2021-11-22 23:33:26 +0100

    Add files via upload
    add files

--- jour07/job02/index.php
+++ jour07/job02/index.php
@@ -1,1 +1,18 @@
+<?php
 
+if (isset($_COOKIE['nbvisites'])){
+    $nbpeople=0;
+    
+}
+else{
+    $nbpeople=$_COOKIE['nbvisites'];
+}
+
+$nbpeople++;
+setcookie(['nbvisites', $nbpeople],time()-3600);
+
+echo $_COOKIE['nbvisites'];
+echo '<form>';
+echo '<input type="button" value="reset">';
+echo '</form>';
+?>
