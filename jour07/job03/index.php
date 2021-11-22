43c65323925eddc695f836f72fff95c3d976739e
fabien-allard <91756104+fabien-allard@users.noreply.github.com>
2021-11-22 23:34:10 +0100

    Add files via upload
    add files

--- jour07/job03/index.php
+++ jour07/job03/index.php
@@ -1,1 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
 
+<?php
+session_start();
+$_SESSION='prenom';
+if (isset($_SESSION['prenom'])){
+    $_SESSION['prenom'] = 0;
+
+} else{
+    $_SESSION['prenom'] ++;
+}
+
+echo $_SESSION['prenom'];
+echo '<form action="index.php">';
+echo '<input type="button" value="reset"';
+echo '</form>';
+
+?>
+
+<form class="box" action="index.php" method="">
+    <h1 class="box-title">Formulaire</h1>
+    <input type="text"  name="prenom" />
+    <input type="submit" name="submit" value="Envoie" />
+</form>
+</body>
+</html>
